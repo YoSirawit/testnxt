5fed83f06e594acf90f1680dc1db5285d2eef3bd
YoSirawit <66070314@kmitl.ac.th>
2024-10-17 19:48:07 +0700

    make it to show the data recenntly add

--- app/page.jsx
+++ app/page.jsx
@@ -6,16 +6,35 @@ import { sql } from '@vercel/postgres';
 import Petinfo from './components/petinfo';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'
+
 async function getPets(){
-    const pets = await sql`SELECT * FROM Pets`
-    return pets;
+  try{
+    const pets = await fetch('http://localhost:3000/api/get-pet-info', {cache: 'no-store'});
+    const data = await pets.json();
+    return data;
+  } catch (error){
+      console.error(error);
+  }
+
+  // const pets = sql`SELECT * FROM Pets`
+  // return pets
+
+
 }
 
+function removeall(){
+  sql`DELETE FROM Pets`
+}
 
 async function Home() {
 
+  // let pets = await getPets();
+  // console.log({pets});
+
   const pets = await getPets();
-  console.log({pets});
+  console.log(pets.pets);
+  
   
   return (
     <div>
@@ -24,16 +43,18 @@ async function Home() {
             <h3>Home</h3>
             <h1><Link href="/api/get-pet-info">Test</Link></h1>
             <Link href="/add-pet">Add pet</Link>
+            
+        </div>
             {
-              pets.rows.map((pet) => {
+              pets.pets.rows.map((pet) => {
                 return(
                   <Petinfo key={pet.name} name={pet.name} owner={pet.owner}/>
                 )})
 
             }
-        </div>
     </div>
   )
 }
 
 export default Home
+
